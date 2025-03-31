7d9ca1575b554f8e2e1df3d07ae867ccc4348358
arthur-d42 <arthurdiness@gmail.com>
2025-03-31 09:38:06 +0200

    change download method to wget

--- download.sh
+++ download.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 cd data/downloaded
-curl https://ftp.ncbi.nih.gov/gene/DATA/gene2pubmed.gz
+wget https://ftp.ncbi.nih.gov/gene/DATA/gene2pubmed.gz
+wget https://ftp.ncbi.nih.gov/gene/DATA/gene_info.gz
