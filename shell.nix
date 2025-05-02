28560c2711dd098725b26fed98446eff9bc22468
arthur-d42 <arthurdiness@gmail.com>
2025-05-02 09:16:32 +0200

    add py4cytoscape

--- shell.nix
+++ shell.nix
@@ -1,15 +1,8 @@
 { pkgs ? import <nixpkgs> {} }:
+
 pkgs.mkShell {
   buildInputs = with pkgs; [
-    python312
-    python312Packages.numpy
-    python312Packages.pip
-    python312Packages.pandas
-    cytoscape
-    python312Packages.pytest
-    python312Packages.networkx
+    uv
   ];
-  shellHook = ''
-    export LD_LIBRARY_PATH=${pkgs.stdenv.cc.cc.lib}/lib:$LD_LIBRARY_PATH
-  '';
+
 }
