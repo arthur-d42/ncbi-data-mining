758cde2e8d7261bded5f59eacf240d4ae5b03868
FHolmstrup <freho@dtu.dk>
2025-05-05 23:46:55 +0200

    Making naming conventions

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 # Importing functions from other files
 from importData import run_import_gene2pubmed
 from processData import run_processing
-# from addName import geneID_to_symbol_dict
+from addName import geneID_to_symbol_dict
 
 
 # Importing data into tmp file for less use of memory 
