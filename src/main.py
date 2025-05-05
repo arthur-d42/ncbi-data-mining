aa77283fb54e20b89ce1ba3faa1855343a3a45b2
FHolmstrup <freho@dtu.dk>
2025-05-05 23:44:07 +0200

    testing

--- src/main.py
+++ src/main.py
@@ -1,10 +1,11 @@
 # Importing functions from other files
 from importData import run_import_gene2pubmed
 from processData import run_processing
+# from addName import geneID_to_symbol_dict
 
 
 # Importing data into tmp file for less use of memory 
-# run_import_gene2pubmed()
+run_import_gene2pubmed()
 
 # Testing
 # run_processing()
